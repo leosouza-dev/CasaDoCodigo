9c0733a5391081334f1bd9f6b67cbbcfb283579f
Leonardo <leonardo@sarival.com.br>
2019-06-05 00:40:46 -0300

    Criado Entidade ItemCarrinho

---
+++ CasaDoCodigo/Models/ItemCarrinho.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+    public class ItemCarrinho
+    {
+        public int Id { get; set; }
+        public Livro Livro { get; set; }
+        public int Quantidade { get; set; }
+        public int CarrinhoCompraId { get; set; }
+    }
+}
